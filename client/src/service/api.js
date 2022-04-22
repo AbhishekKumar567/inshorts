58acc33187bbb5ccff568f7de1dff09bc6423356
AbhishekKumar567 <abhiku019@gmail.com>
2022-04-22 22:03:07 +0530

    first commit

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -6,7 +6,7 @@ const URL = "https://ichat-apps.herokuapp.com/"
 export const getNews = async (page,size=5) =>{
 
     try{
-        return await axios.get(`${URL}/news?page=${page}&size=${size}`)
+        return await axios.get(`${URL}news?page=${page}&size=${size}`)
 
     }
     catch(e){
